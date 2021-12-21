b718a719814f233d6d2b3e9472503dbc6f1f0e1b
vlad <vlad.shuisky@yandex.ru>
2021-12-21 10:34:32 +0300

    adding ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Counter from './Components/Counter'
+import PostItem from './Components/PostItem'
+import PostList from './Components/PostList';
+import MyButton from './Components/UI/button/MyButton';
+import './Styles/App.css'
 
 function App() {
+  const [posts , setPosts] = useState(
+    [
+      {id: 1, title: 'JavaScript', description: "JavaScript - Это язык программирования высокого уровня"},
+      {id: 2, title: 'JavaScript', description: "Python - Это язык программирования высокого уровня"},
+      {id: 3, title: 'JavaScript', description: "Java - Это язык программирования высокого уровня"},
+      {id: 4, title: 'JavaScript', description: "Ruby - Это язык программирования высокого уровня"},
+    ]
+  )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type='text' placeholder='Название поста'/>
+        <input type='text' placeholder='Содержание поста'/>
+        <MyButton>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про языки программирования" />  
     </div>
   );
 }
